5a90ae38ab700989a5107678637a58f4bda762d8
red093 <luca.red93@gmail.com>
2024-08-18 17:25:24 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-}
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
